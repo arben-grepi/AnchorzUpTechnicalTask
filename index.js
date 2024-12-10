942ce8d3ccee8ef04e459d64bccb3151c36582d1
arben-grepi <arben.grepi@gmail.com>
2024-12-11 00:10:47 +0200

    ebabled helmet, enabled morgan in development environment

--- index.js
+++ index.js
@@ -3,11 +3,22 @@ require("dotenv").config(); // Only call this once at the entry point
 
 const connectDB = require("./db/MongoDBConnect");
 const express = require("express");
+const morgan = require("morgan"); // HTTP request logging middleware
+const helmet = require("helmet"); // Security middleware
+
+const debug = require("debug"); // Debugging utility
+const log = debug("app:log");
 
 const app = express();
 
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  log("Morgan enabled...");
+}
+
 // Middleware
 app.use(express.json());
+app.use(helmet());
 
 // Start Server and Database connection
 (async () => {
