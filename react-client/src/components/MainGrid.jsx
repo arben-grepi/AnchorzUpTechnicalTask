62e78b80a316106a1374b6b276acfec85c3c24ef
arben-grepi <arben.grepi@gmail.com>
2024-12-12 19:02:49 +0200

    I set the styling, final touch

--- react-client/src/components/MainGrid.jsx
+++ react-client/src/components/MainGrid.jsx
@@ -1,25 +1,29 @@
 import { Flex, Box, Stack } from "@chakra-ui/react";
 // import { useState } from "react";
 
-// import { AddUrlButton } from "./SubComponents/AddUrlButton";
-// import { InputUrl } from "./SubComponents/InputUrl";
+import InputUrl from "./SubComponents/InputUrl";
+import DropDownMenu from "./SubComponents/DropDownMenu";
+import AddUrlButton from "./SubComponents/AddUrlButton";
 
 const Main = () => {
   return (
     <>
-      <Stack>
-        <h1>Main</h1>
-        <Flex className="MainGrid-Flex">
-          <Box>
-            <p>TextBox</p>
+      <Stack width={"100%"}>
+        <h1 className="MainHeading">Main</h1>
+        <Stack className="MainGrid-Stack" width={"100%"}>
+          <div></div>
+          <Flex className="MainGrid-Flex">
+            <Box width={"60%"}>
+              <InputUrl />
+            </Box>
+            <Box>
+              <DropDownMenu />
+            </Box>
+          </Flex>
+          <Box paddingTop={"2rem"} id="AddUrlButton">
+            <AddUrlButton />
           </Box>
-          <Box>
-            <p>alasvetolaatikko</p>
-          </Box>
-        </Flex>
-        <Box paddingTop={"2rem"} id="AddUrlButton">
-          Nappi
-        </Box>
+        </Stack>
       </Stack>
     </>
   );
