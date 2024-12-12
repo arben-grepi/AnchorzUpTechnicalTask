b9f599a63f3022119eb99c0a974c67a42495c0ec
arben-grepi <arben.grepi@gmail.com>
2024-12-12 02:38:24 +0200

    added the grid layout

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "./index.css";
+import { VStack } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <div className="grid-container">
+        <div className="grid panel-grid">
+          <VStack class="VStack-panel">
+            <h2>Panel</h2>
+            <div id="shortlinks"></div>
+          </VStack>
+        </div>
+        <div className="grid main-grid">
+          <VStack class="VStack-main">
+            <h1>Header</h1>
+            <h2>Main</h2>
+            <h2>Main</h2>
+          </VStack>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
