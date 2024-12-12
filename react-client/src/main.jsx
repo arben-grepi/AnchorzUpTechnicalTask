cbddd8e743ce95bf01c2187523ae6ef0f2dec280
arben-grepi <arben.grepi@gmail.com>
2024-12-12 02:37:51 +0200

    pasted index.css from other project, added clobal colors to the theme config

--- react-client/src/main.jsx
+++ react-client/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { ChakraProvider, createSystem, defaultConfig } from "@chakra-ui/react";
+
+const system = createSystem(defaultConfig, {
+  theme: {
+    colors: {
+      primary: "#92278f",
+      linkText: "#179be5",
+      lightGrayText: "#878686",
+      heading: "#000000",
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChakraProvider value={system}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
