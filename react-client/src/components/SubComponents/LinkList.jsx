9d2286003401df0a95ab5747186ef4bb73652ac3
arben-grepi <arben.grepi@gmail.com>
2024-12-12 23:09:44 +0200

    Modified LInklist

---
+++ react-client/src/components/SubComponents/LinkList.jsx
@@ -0,0 +1,51 @@
+// LinkList.jsx
+/* eslint-disable react/prop-types */
+import { Flex, Box } from "@chakra-ui/react";
+import { Tooltip } from "@/components/ui/tooltip";
+
+const LinkList = ({ items, onDelete, onLinkClick }) => {
+  const handleDelete = (shortId) => {
+    onDelete(shortId);
+  };
+
+  const handleLinkClickInternal = (qrCode) => {
+    // Kutsutaan parentin onLinkClick-funktiota, annetaan qrCode
+    if (onLinkClick) {
+      onLinkClick(qrCode);
+    }
+  };
+
+  return (
+    <Flex direction="column" gap="1rem" width={"100%"}>
+      {items.map((item) => (
+        <Flex key={item.shortId} justify="space-between" align="center">
+          <Box paddingLeft={"30px"}>
+            <Tooltip size="xl" content={item.originalUrl}>
+              <a
+                className="shortlink-text"
+                href={item.originalUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                onClick={() => handleLinkClickInternal(item.qrCode)}
+              >
+                www.shortid.com/{item.shortId}
+              </a>
+            </Tooltip>
+          </Box>
+          <Box>
+            <span
+              role="img"
+              aria-label="Delete"
+              onClick={() => handleDelete(item.shortId)}
+              style={{ cursor: "pointer", paddingRight: "30px" }}
+            >
+              🗑️
+            </span>
+          </Box>
+        </Flex>
+      ))}
+    </Flex>
+  );
+};
+
+export default LinkList;
