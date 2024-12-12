082b2e4fcd8a22167f608ba937eb0e5ffc6c0565
arben-grepi <arben.grepi@gmail.com>
2024-12-12 05:32:11 +0200

    removed part that are no longer needed, added cors

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
 import "./config.js"; // Ensure dotenv is loaded before any other imports
-
 import connectDB from "./db/MongoDBConnect.js";
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import debug from "debug";
+import cors from "cors"; // Import CORS
 import urlRoutes from "./routes/urls.js";
 
 const log = debug("app:log");
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const app = express();
-
-// Serve static files from the "Client" directory
-app.use(express.static(join(__dirname, "Client"))); // Correct path
-
-// Log for debugging
-console.log("Serving static files from:", join(__dirname, "Client"));
 
 if (app.get("env") === "development") {
   app.use(morgan("tiny"));
@@ -29,6 +18,7 @@ if (app.get("env") === "development") {
 
 app.use(express.json());
 app.use(helmet());
+app.use(cors()); // Enable CORS
 
 // API routes
 app.use("/api/v1/urls", urlRoutes);
