de919cf35d64af8a3c8701ddaac2acca3762dc5e
arben-grepi <arben.grepi@gmail.com>
2024-12-12 05:39:02 +0200

    removed imports that are not needed

--- react-client/src/components/LinkList.jsx
+++ react-client/src/components/LinkList.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/prop-types */
-import React from "react";
 import { Flex, Box } from "@chakra-ui/react";
 
 const YourComponent = ({ items }) => {
--- react-client/src/components/Panel.jsx
+++ react-client/src/components/Panel.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { GlobalContext } from "../context/GlobalState";
 import { useContext, useEffect } from "react";
 
